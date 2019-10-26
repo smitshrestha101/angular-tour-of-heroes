4e3d525aa95780f74208c2f7670f23e5338b154a
Smit Shrestha <smitshrestha101@gmail.com>
2019-10-26 16:27:53 -0500

    installed routing services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,12 @@ export class HeroService {
     this.messageService.add('HeroService: fetched Heros');
     return of(HEROES);
   }
+  
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
   constructor(private messageService: MessageService) { }
 }
